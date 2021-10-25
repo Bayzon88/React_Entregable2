eef87281cb9d4d3fc775cf2f33a9f21b8ddcc4d0
Bayzon88 <beltranalvaro08@gmail.com>
2021-10-25 18:50:56 -0500

    correcciones

--- src/Components/ItemList.js
+++ src/Components/ItemList.js
@@ -3,73 +3,34 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
 
-const ITEMS = [
-  {
-    id: 1,
-    nombre: "articulo1",
-    descripcion: "Descripcion generica",
-    sku: "1234567890",
-    precio: 10,
-  },
-  {
-    id: 2,
-    nombre: "articulo2",
-    descripcion: "Descripcion generica",
-    sku: "1234567890",
-    precio: 20,
-  },
-  {
-    id: 3,
-    nombre: "articulo3",
-    descripcion: "Descripcion generica",
-    sku: "1234567890",
-    precio: 50,
-  },
-  {
-    id: 4,
-    nombre: "articulo4",
-    descripcion: "Descripcion generica",
-    sku: "1234567890",
-    precio: 30,
-  },
-];
-
 //Carga items incorporando datos al card usando el array ITEMS
-function ItemList() {
+function ItemList({ productos }) {
   //HOOK useState
-  const [card, setCard] = useState(() => {
-    return (
-      <div
-        style={{
-          color: "white",
-          width: "100vw",
-          fontSize: "50px",
-          textAlign: "center",
-        }}
-      >
-        Aqui apareceran los componentes
-      </div>
-    );
-  });
 
-  const SetCardInfo = () => {
-    setCard(
-      ITEMS.map((item) => {
-        /*spread operator para no tener que escribir todos los nombres de los 
-          props nombre={nombre} descripcion={descripcion} etc*/
-        return <Item {...item}></Item>;
-      })
-    );
+  const setCardInfo = () => {
+    return productos.map((item) => {
+      return <Item {...item}></Item>;
+    });
   };
 
-  //CREANDO PROMISE CON TIME OUT
-  const TAREA = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(SetCardInfo());
-    }, 2000);
-  });
-
-  return <Container className='d-flex'>{card}</Container>;
+  return (
+    <Container className='d-flex'>
+      {productos.length === 0 ? (
+        <div
+          style={{
+            color: "white",
+            width: "100vw",
+            fontSize: "50px",
+            textAlign: "center",
+          }}
+        >
+          Aqui apareceran los componentes
+        </div>
+      ) : (
+        setCardInfo()
+      )}
+    </Container>
+  );
 }
 
 export default ItemList;
--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,10 +1,56 @@
 import ItemList from "./ItemList";
 import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react";
+
+const ITEMS = [
+  {
+    id: 1,
+    nombre: "articulo1",
+    descripcion: "Descripcion generica",
+    sku: "1234567890",
+    precio: 10,
+  },
+  {
+    id: 2,
+    nombre: "articulo2",
+    descripcion: "Descripcion generica",
+    sku: "1234567890",
+    precio: 20,
+  },
+  {
+    id: 3,
+    nombre: "articulo3",
+    descripcion: "Descripcion generica",
+    sku: "1234567890",
+    precio: 50,
+  },
+  {
+    id: 4,
+    nombre: "articulo4",
+    descripcion: "Descripcion generica",
+    sku: "1234567890",
+    precio: 30,
+  },
+];
 
 function ItemListContainer() {
+  const [productos, setProductos] = useState([]);
+  //CREANDO PROMISE CON TIME OUT
+  const TAREA = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(ITEMS);
+    }, 2000);
+  });
+  useEffect(() => {
+    TAREA.then((result) => {
+      setProductos(result);
+      console.log(result);
+    });
+  }, []);
+
   return (
     <Container>
-      <ItemList></ItemList>;
+      <ItemList productos={productos}></ItemList>;
     </Container>
   );
 }
